296fb33001bdc5c19e5c89b11f7d5cd4ab5da041
washubeta <washubetawebmaster@gmail.com>
2012-05-18 15:48:16 -0500

    update composite to be responsive

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(function() {
-  $("#comp").mousemove(function(e) {
-    var x = (100 * (e.pageX - this.offsetLeft)) / $("#comp").width();
-    var y = (100 * (e.pageY - this.offsetTop)) / $("#comp").height();
+  $("#composite").mousemove(function(e) {
+    var x = (100 * (e.pageX - this.offsetLeft)) / $("#composite").width();
+    var y = (100 * (e.pageY - this.offsetTop)) / $("#composite").height();
     $("#zoom").css("background-position", "" + x + "% " + y + "%");
   });
 });
